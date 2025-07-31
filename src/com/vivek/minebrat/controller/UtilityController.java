2a0ce581cf746ea6e163d2468be84cd8f8fabf7c
vivek <kvivek5003@gmail.com>
2025-07-31 14:06:12 +0530

    Add full UserController implementation

--- src/com/vivek/minebrat/controller/UtilityController.java
+++ src/com/vivek/minebrat/controller/UtilityController.java
@@ -1,5 +1,28 @@
 package com.vivek.minebrat.controller;
 
-public class UtilityController {
+import com.vivek.minebrat.dto.*;
+import com.vivek.minebrat.entity.User;
+import com.vivek.minebrat.service.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UtilityController {
+    private final PermutationService permutationService;
+
+    @GetMapping("/permutations")
+    public ResponseEntity<Set<String>> getPermutations(@RequestParam String input) {
+        return ResponseEntity.ok(permutationService.permute(input));
+    }
 }
